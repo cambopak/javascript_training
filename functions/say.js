8abfa4490b0bdcb9ded02993f159553272fbf012
zohibt <zohib12@gmail.com>
2021-12-16 12:50:39 -0500

    added function folder and say.js

---
+++ functions/say.js
@@ -0,0 +1,14 @@
+/*console.log("hello");
+console.log("hi);
+console.log("how do you do");
+console.log("Quite all right");i*/
+
+function say(words) {
+  console.log(words);
+}
+
+say("hello");
+say("hi");
+say("how are you");
+say("I'm fine");
+
