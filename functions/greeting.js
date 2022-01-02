24d9a212a2fcd3733eab3243b3e2331a8f75aef7
zohibt <zohib12@gmail.com>
2022-01-02 13:41:14 +0000

    updated

--- functions/greeting.js
+++ functions/greeting.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 function greetPeople() {
   let greetingMessage = "Good Morning!";
-=======
-let greetingMessage = "Good Morning";
-
-function greetPeople() {
->>>>>>> 8d67a1c91c19576230a2af633a250a58484d140e
   console.log(greetingMessage);
 }
 
 greetPeople();
-<<<<<<< HEAD
 console.log(greetingMessage); // raises ReferenceError
-=======
->>>>>>> 8d67a1c91c19576230a2af633a250a58484d140e
