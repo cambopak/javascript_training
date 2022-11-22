17d265204b70734b3740886f37ec0914b44ddbc9
Zohib <94336926+zohibt@users.noreply.github.com>
2022-11-22 14:49:24 -0500

    Logical Conditions 3

--- functions/working.js
+++ functions/working.js
@@ -1,5 +1,7 @@
-if (true && false) {
-  console.log('Yes!');
-} else {
-  console.log('No...');
-}
+let sale = true;
+let admissionPrice = !sale ? 5.25 : 3.99;
+
+console.log('$' + admissionPrice);
+
+
+$3.99
