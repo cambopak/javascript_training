e22e5c427eb60ad003a832351f016885cd0d9bd0
Zohib <94336926+zohibt@users.noreply.github.com>
2022-11-23 12:01:55 -0500

    Three-way comparison

--- functions/working.js
+++ functions/working.js
@@ -1,10 +1,15 @@
-function multiplesOfThree() {
-    let divisor = 1;
-  
-    for (let dividend = 3; dividend <= 30; dividend += 3) {
-      console.log(dividend + ' / ' + divisor + ' = 3');
-      divisor += 1;
+ // -1
+//compareByLength('strength', 'dignity');      //  1
+//compareByLength('humor', 'grace');           //  0
+
+function compareByLength(str1, str2) {
+    if (str1.length < str2.length) {
+      return -1;
+    } else if (str1.length > str2.length) {
+      return 1;
+    } else {
+      return 0;
     }
   }
-  
-multiplesOfThree();
+
+  compareByLength('patience', 'perseverance');
